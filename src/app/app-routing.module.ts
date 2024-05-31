85facde7cc4eda4d064617d922f09a98f5a6d82e
Edilson <edilsonhuaman44@gmail.com>
2024-05-30 23:35:51 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ProductoComponent } from './components/producto/producto.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
+import { ContactenosComponent } from './components/contactenos/contactenos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'inicio', component: InicioComponent },
+  { path: 'nosotros', component: NosotrosComponent },
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'producto', component: ProductoComponent },
+  { path: 'marcas', component: MarcasComponent },
+  { path: 'contactenos', component: ContactenosComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
